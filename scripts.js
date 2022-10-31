043c52d413b318b5f3bc26b2bc2bebee2ed06129
Jon-Michael <jof8888@gmail.com>
2022-10-30 21:20:52 -0500

    Revert "added javascript and fixed small part of css"
    This reverts commit 45c441034ea26ee733f7f33b6c14c69a3fafde30.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,1 @@
 /* Add your JavaScript to this file */
-window.onload= ()=>{
-  let btn = document.querySelector(".newsletter form button.btn");
-  let mail = document.querySelector(".newsletter form input[type=\"email\"]");
-  let message = document.querySelector(".newsletter div.message");
-  const testmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ ;
-  
-  mail.setAttribute("placeholder", "Email address"); //swet to match template
-
-  btn.onclick = (event)=>{
-    event.preventDefault();
-    if (testmail.test(mail.value.toLowerCase()) != 0 && mail.value.length){
-      message.innerHTML = "Thank you! Your email address ${mail.value} has been added to our mailing list!";
-    }
-    else{
-      message.innerHTML = "Please enter a valid email address.";
-    }
-  }
-}
